cd5a15fce66d1c7d97879ddf2ae1313612c8ed18
Digiox <bourlon.lyann84@gmail.com>
2022-09-13 03:34:21 +0200

    Added path dependecy to file /bin/stop.js

--- bin/stop.js
+++ bin/stop.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
 var shell = require("shelljs");
+const path = require("path")
+
 const composeFile = path.resolve(__dirname, "../docker-compose.yml");
 shell.exec("docker compose -f " + composeFile + " down");
