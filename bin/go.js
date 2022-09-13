26411be6e2c21e3fb6e5fde435750c7114ad0795
Digiox <bourlon.lyann84@gmail.com>
2022-09-13 03:18:28 +0200

    edited bin/go.js

--- bin/go.js
+++ bin/go.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 
 var shell = require("shelljs");
-shell.exec("docker compose --env-file=.env up -d");
+shell.exec("docker compose -f docker-compose.yml --env-file=.env up -d");
